18ecbb9c6a2d020ad9480988dd78b2dbcb34fc68
Domi.H <dominique@hassenpflug.ch>
2026-07-04 12:29:01 +0200

    feat: Projekt-Setup mit pom.xml Dependencies

---
+++ src/main/java/ch/wiss/mealmapbackend/MealmapBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.mealmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealmapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealmapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/wiss/mealmapbackend/MealmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.mealmapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
