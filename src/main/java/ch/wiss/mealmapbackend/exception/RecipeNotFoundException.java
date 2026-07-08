2f82b2208adf858354c9922e241e628a278d5a67
MaryTiiaa <marina.tschirky@thread-it.ch>
2026-07-09 00:20:39 +0200

    feat: RecipeNotFoundException implementiert

--- src/main/java/ch/wiss/mealmapbackend/exception/RecipeNotFoundException.java
+++ src/main/java/ch/wiss/mealmapbackend/exception/RecipeNotFoundException.java
@@ -1,4 +1,8 @@
 package ch.wiss.mealmapbackend.exception;
 
-public class RecipeNotFoundException {
+public class RecipeNotFoundException extends RuntimeException {
+
+    public RecipeNotFoundException(Long id) {
+super("Kein Rezept mit der ID " + id + " gefunden!");
+    }
 }
