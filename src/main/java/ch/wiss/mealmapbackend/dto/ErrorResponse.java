4281372c218ddc542de99d5416daa6536f996fad
MaryTiiaa <marina.tschirky@thread-it.ch>
2026-07-10 11:12:51 +0200

    feat: ErrorResponse DTO hinzugefuegt

---
+++ src/main/java/ch/wiss/mealmapbackend/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package ch.wiss.mealmapbackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String message,
+        Map<String, String> fieldErrors
+){}
