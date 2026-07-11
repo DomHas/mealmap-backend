ad7a973c9ffb9a0f95eac79a93cba9b957c6d3d9
MaryTiiaa <marina.tschirky@thread-it.ch>
2026-07-11 08:46:13 +0200

    docs: JavaDoc fuer RecipeNotFoundException ergaenzt

--- src/main/java/ch/wiss/mealmapbackend/exception/RecipeNotFoundException.java
+++ src/main/java/ch/wiss/mealmapbackend/exception/RecipeNotFoundException.java
@@ -1,8 +1,15 @@
 package ch.wiss.mealmapbackend.exception;
 
+/**
+ * Wird geworfen, wenn ein Rezept mit der angegebenen ID
+ * nicht in der Datenbank gefunden werden kann.
+ */
 public class RecipeNotFoundException extends RuntimeException {
 
+    /**
+     * @param id die ID des nicht gefundenen Rezepts
+     */
     public RecipeNotFoundException(Long id) {
-super("Kein Rezept mit der ID " + id + " gefunden!");
+        super("Kein Rezept mit der ID " + id + " gefunden!");
     }
 }
